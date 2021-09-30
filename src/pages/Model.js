3fc1e77fb544ef29f5d1a7c1d8a32004b7a4188f
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-09-30 10:34:35 +0530

    Home Page Structure

---
+++ src/pages/Model.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Model() {
+    return (
+        <>
+            <main>
+                <div className="container">
+                    <div className="row center">
+                        <div className="image container">
+                            <div className="thumbnail">
+                                <div className="frame">
+                                    <Link>
+
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Model
